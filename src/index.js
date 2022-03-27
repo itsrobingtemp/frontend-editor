35a0ce0bd4baf6c0f1957c1516d3e6b6f5850fd7
Robin Granqvist <itsrobing@gmail.com>
2022-03-27 23:44:56 +0300

    Working...

--- src/index.js
+++ src/index.js
@@ -4,11 +4,6 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  // <React.StrictMode>
-  // </React.StrictMode>,
-  <App />,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 reportWebVitals();
